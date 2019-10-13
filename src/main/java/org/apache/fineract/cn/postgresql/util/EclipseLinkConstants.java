239888a4ee99637dff70096424cc22a1ca3d3342
Juhan Aasaru <Juhan.Aasaru@nortal.com>
2019-10-13 19:18:14 +0300

    default to false

--- src/main/java/org/apache/fineract/cn/postgresql/util/EclipseLinkConstants.java
+++ src/main/java/org/apache/fineract/cn/postgresql/util/EclipseLinkConstants.java
@@ -22,5 +22,5 @@ public interface EclipseLinkConstants {
 
   String ECLIPSE_LINK_SHOW_SQL = "eclipseLink.showSql";
 
-  String ECLIPSE_LINK_SHOW_SQL_DEFAULT = "true";
+  String ECLIPSE_LINK_SHOW_SQL_DEFAULT = "false";
 }
